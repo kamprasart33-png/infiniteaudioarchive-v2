26960325d9b61f8efd956c9e36d9efd1d319fbe2
kamprasart33-png <kamprasart33@gmail.com>
2026-08-21 11:07:23 -0700

    Create Home.jsx

---
+++ src/src/ui/pages/Home.jsx
@@ -0,0 +1,4 @@
+import { classifyMood } from "../../ai/mood/moodModel.js";
+import { clusterGenres } from "../../ai/genre/genreCluster.js";
+import { generateEmbedding } from "../../ai/semantic/embeddings.js";
+import { runAIPipeline } from "../../ai/pipeline/aiPipeline.js";
