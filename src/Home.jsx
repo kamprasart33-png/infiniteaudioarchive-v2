1f5effd85719cec985424d5954f47924873c6ff5
kamprasart33-png <kamprasart33@gmail.com>
2026-08-22 07:34:59 -0700

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import AudioUpload from "./components/AudioUpload";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>InfiniteAudioArchive v2</h1>
+      <AudioUpload />
+    </div>
+  );
+}
